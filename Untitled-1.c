744f292e63b1ab01e1251bd7adedf0827affafd3
seu_usuario_no_github <adenilsonalves1907@gmail.com>
2023-09-01 23:40:53 -0300

    produto simples

---
+++ Untitled-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int valor1, valor2;
+    int prod;
+    
+    scanf("%d", &valor1);
+    scanf("%d", &valor2);
+
+    prod = valor1 * valor2;
+    
+    printf("PROD = %d\n", prod);
+    
+    return 0;
+}
