b087c1afa635180d040ac1180c8e17da31bece94
denaalves <adenilsonalves1907@gmail.com>
2023-09-02 13:20:56 -0300

    area do circulo

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
- 
-int main() {
- 
-    int valor1, valor2;
-    int prod;
-    
-    scanf("%d", &valor1);
-    scanf("%d", &valor2);
+#include <iostream>
+#include <cmath>
 
-    prod = valor1 * valor2;
-    
-    printf("PROD = %d\n", prod);
-    
+int main() {
+    const double pi = 3.14159; // Valor de pi
+    double raio, area, valorAdicional;
+
+    //std::cout << "Digite o valor do raio do circulo: ";
+    std::cin >> raio;
+
+    //std::cout << "Digite o valor adicional para somar a area: ";
+    std::cin >> valorAdicional;
+
+    // Calcula a area do circulo usando a formula A = pi * r^2
+    area = pi * std::pow(raio, 2);
+
+    // Soma o valor adicional à área
+    area += valorAdicional;
+
+    printf("A=%.4f\n", area);
+
     return 0;
 }
